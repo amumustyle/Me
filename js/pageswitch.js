72ba2f9df9d172d426955558872bbb1172edae9c
amumustyle <719578460@qq.com>
2016-07-22 21:14:44 +0800

    newme

--- js/pageswitch.js
+++ js/pageswitch.js
@@ -3,7 +3,7 @@
 		'container' : '#container',//容器
 		'sections' : '.section',//子容器
 		'easing' : 'ease',//特效方式，ease-in,ease-out,linear
-		'duration' : 1000,//每次动画执行的时间
+		'duration' : 1700,//每次动画执行的时间
 		'pagination' : true,//是否显示分页
 		'loop' : false,//是否循环
 		'keyboard' : true,//是否支持键盘
